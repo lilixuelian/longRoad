21eef80e048585dafdf1127161e0441e4b85f9f4
jinjianxing <didnelpsun@163.com>
2019-11-19 12:56:39 +0800

    Enter

--- src/pages/Desc/Desc11/Desc11.jsx
+++ src/pages/Desc/Desc11/Desc11.jsx
@@ -6,12 +6,13 @@ import '../Desc.scss'
 import place from '../../../pictures/iconphoto/place.png'
 import time from '../../../pictures/iconphoto/time.png'
 import content from '../../../pictures/iconphoto/content.png'
+import sense from '../../../pictures/iconphoto/sense.png'
 import One from '../../../components/Number/One/One'
 import Two from '../../../components/Number/Two/Two'
-import Three from '../../../components/Number/Three/Three'
-import Four from '../../../components/Number/Four/Four'
-import Five from '../../../components/Number/Five/Five'
-import Six from '../../../components/Number/Six/Six'
+// import Three from '../../../components/Number/Three/Three'
+// import Four from '../../../components/Number/Four/Four'
+// import Five from '../../../components/Number/Five/Five'
+// import Six from '../../../components/Number/Six/Six'
 // import Seven from '../../../components/Number/Seven/Seven'
 // import Eight from '../../../components/Number/Eight/Eight'
 // import Nine from '../../../components/Number/Nine/Nine'
@@ -22,17 +23,17 @@ export default class Desc11 extends Component {
     navigationBarTitleText: '吴起会师'
   }
 
-  componentWillMount () { }
+  componentWillMount() { }
 
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  render () {
+  render() {
     return (
       <View className='Desc'>
         <View>　</View>
@@ -52,10 +53,6 @@ export default class Desc11 extends Component {
           <View> 　　吴起镇相传为战国时魏国大将吴起（约前400年）屯兵驻守而得名。1935年，毛泽东率领的中央红军长征到达陕北，吴起镇成为长征落脚点而名播中外。该镇历史悠久，
             胜利山因“割尾巴”战役而闻名天下，新窑院设有毛主席纪念馆，是革命老区，历史重镇。\n
           </View>
-          <View> 　　第三道封锁线设在粤汉铁路沿湘粤边湖南境内良田到宜章之间；\n
-          </View>
-          <View> 　　第四道封锁线就是湘江。
-          </View>
         </AtCard>
         <View>　</View>
         <AtCard
@@ -63,42 +60,20 @@ export default class Desc11 extends Component {
           thumb={content}
         >
           <One></One>
-          <View> 　　1933年9月至1934年夏，中央苏区红军进行第五次反“围剿”作战，由于中共中央领导人博古（秦邦宪）和共产国际派来的军事顾问李德，先是实行冒险主义的进攻战略，
-            后又实行保守主义的防御战略，致使红军屡战失利，苏区日渐缩小。\n</View>
-          <View> 　　1934年4月，中央红军（红一方面军改称）在江西省广昌与国民党军进行决战，损失严重，形势危殆。5月，中央书记处作出准备将中央红军主力撤出根据地的决定，
-            后获共产国际同意。\n</View>
+          <View> 　　红1方面军主力通过腊子口后，继续北进，于1935年10月19日到达陕甘苏区吴起镇，与陕甘革命根据地红军胜利会师，随后又同十五军团红军胜利会师。至此，中央红军胜利地完成了历时一年，
+            纵横11个省，行程2.5万里的长征。党中央和中央红军主力终于找到了长征立足点，抵达最后的目的地，胜利地实现了历史性的战略转移。\n</View>
           <View> 　　\n</View>
           <Two></Two>
-          <View> 　　为筹划战略转移事宜，成立了由博古、李德和周恩来组成的“三人团”。7月，中华苏维埃共和国中央革命军事委员会命令红7军团组成北上抗日先遣队，向闽浙皖赣边挺进；
-红6军团从湘赣苏区突围西征，到湘中发展游击战争，意在调动国民党“围剿”军，以减轻中央苏区的压力，但未能达到目的。\n</View>
-          <View> 　　10月初，国民党军向中央苏区的中心区域进攻，迅速占领了兴国、宁都、石城一线。红军的机动回旋余地更加缩小，在苏区内打破国民党军的“围剿”已无可能，
-            于是被迫退出苏区，进行战略转移。\n</View>
-          <View> 　　10月10日，中共中央、中革军委率领中央红军主力8.6万余人，从江西省瑞金、古城等地出发，开始了著名的长征。按照预定计划，
-            中央红军准备转移到湘西地区同红2军团、红6军团会合。\n</View>
-          <View> 　　\n</View>
-          <Three></Three>
-          <View> 　　10月17日，中央红军由雩都（今于都）南渡贡水。21日，中革军委以红1军团为左路前卫，红3军团为右路前卫，红9军团掩护左翼，红8军团掩护右翼；
-中央和军委机关及直属队编成的两个纵队居中，红5军团担任后卫，从王母渡、新田之间突破国民党军第一道封锁线，于25日全部渡过信丰河（桃江）。\n</View>
-          <View> 　　\n</View>
-          <Four></Four>
-          <View> 　　此后，中央红军继续西进，至11月15日，先后从湖南省汝城以南的天马山至广东省的城口间、湖南省的良田至宜章间通过第二、第三道封锁线，
-            进至临武、蓝山、嘉禾地区。\n</View>
-          <View> 　　\n</View>
-          <Five></Five>
-          <View> 　　这时，蒋介石已判明红军转移的战略意图，遂任命何键为“追剿”军总司令，指挥16个师分五路“追剿”；
-同时令桂军5个师、粤军4个师及黔军一部进行堵截，总兵力近30万人，企图将中央红军歼灭于湘江以东地区。\n</View>
-          <View> 　　11月25日，中革军委决定，中央红军分四路纵队，从全州、兴安间西渡湘江，突破国民党军第四道封锁线，前出到湘桂边境的西延地区。
-            当日，红1军团、红3军团击破桂军的阻击进入桂北。\n</View>
-          <View> 　　27日，先头部队红2军团、红4师各一部渡过湘江，控制了脚山铺至界首间30公里的湘江两岸渡口，但后续部队因辎重过多，道路狭窄，
-            行动迟缓，未能赶到渡口。\n</View>
-          <View> 　　此时，国民党“追剿”军进至全州、零陵、道县、东安地区；桂军5个师开至灌阳、兴安一线。为掩护中央、军委纵队及后续军团渡过湘江；\n</View>
-          <View> 　　红1军团、红3军团在桂北湘江两岸的新圩、脚山铺、光华铺等地区，红5军团在永安关、水车一带与国民党军血战五昼夜，
-            于12月1日保护中央机关渡过湘江。\n</View>
-          <View> 　　红5军团第34师被阻于湘江东岸，转战于灌阳、道县一带，虽经英勇战斗，予敌重大杀伤，终因寡不敌众，弹尽粮绝，
-            包括师长陈树湘在内的绝大多数指战员牺牲。此时，中央红军已折损过半，兵力锐减为3万余人。</View>
-          <View> 　　\n</View>
-          <Six></Six>
-          <View> 　　湘江战役，中央红军经过英勇奋战，突破了国民党军第四道封锁线，挫败了蒋介石企图歼灭红军于湘江以东的计划。</View>
+          <View> 　　10月22日，中共中央在此召开政治局扩大会议，宣告中央红军长征胜利结束。参加当天会议的有：张闻天、博古、毛泽东、王稼祥、周恩来、邓发、李富春、聂荣臻、刘少奇、叶剑英、凯丰、贾拓夫、彭德怀等。
+            会议的中心议程是：总结俄界会议后红军的行动，确定新形势下陕甘支队的行动方针。会议批准了榜罗镇会议把红军长征落脚点放在陕北的战略决策，决定党和红军今后的战略任务是建立西北苏区，以领导全国革命，
+            从而宣告了中央红军长征的完结，开创了党中央全国革命大本营放在陕北的新的历史时期。\n</View>
+         </AtCard>
+        <View>　</View>
+        <AtCard
+          title='意义'
+          thumb={sense}
+        >
+          <View> 　　吴起镇会师标志着红一方面军主力胜利地完成历时一年、纵横11个省、行程二万五千里的长征，开始了为实现党中央新的战略任务而斗争的新阶段。\n</View>
         </AtCard>
         <View>　</View>
       </View>
